c2566b0d7e795589bd9fe0a899f718bad2e064bd
MrTroglodita-design <agustin.juarez98@outlook.com>
2025-06-15 20:06:03 -0300

    test

---
+++ tpi-prog-i/funciones.h
@@ -0,0 +1,11 @@
+#pragma once
+
+void juego(int &puntaje, int &player);
+
+void turno(int &stockP1, int &stockP2, int &puntosP1, int &puntosP2);
+
+void juegoP1(int &stockP1, int &stockP2, int &puntosP1, int &datosTotP1);
+
+void juegoP2(int &stockP2, int &stockP1, int &puntosP2, int &datosTotP2);
+
+void eleccionP1(int &numObjetivo, int &puntosP1, int &stockP1, int &stockP2);
